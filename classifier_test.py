1e626e0259811dc3626adce17a38400f382e5b60
gerkenma <mark.gerken@slu.edu>
2018-05-01 15:15:19 -0500

    Linear Decisiom Commit
    Linear classifier prepared. Can easily swap between full and sample files. Currently outputs accuracy and confusion matrix. Preparing to attempt KNN approach.

---
+++ classifier_test.py
@@ -0,0 +1,44 @@
+from sklearn.metrics import accuracy_score
+import scipy.sparse as sp
+import numpy as np
+from sklearn import datasets
+from sklearn import svm
+from sklearn import metrics
+from sklearn import neighbors
+
+TRAIN_LENGTH = 3
+TEST_LENGTH = 1
+
+
+def classifier_test(classifier, source):
+    # training = np.random.random_integers(0, 89, TRAIN_LENGTH)
+    training = [0, 1, 2]
+    print("Training days:", training)
+    X, y = datasets.load_svmlight_file(source + "/Day" + str(training[0]) + ".svm", n_features=3300000)
+    for day in training[1:]:
+        data = datasets.load_svmlight_file(source + "/Day" + str(day) + ".svm", n_features=3300000)
+        X = sp.vstack((X, data[0]), format='csr')
+        y = np.concatenate((y, data[1]), axis=0)
+
+    print("Beginning fitting.")
+    classifier.fit(X, y)
+    print("Done fitting.")
+
+    # testing = np.random.random_integers(90, 120, TEST_LENGTH)
+    testing = [90]
+    print("Testing days:", testing)
+    test_X, test_y = datasets.load_svmlight_file(source + "/testData/Day" + str(testing[0]) + ".svm", n_features=3300000)
+    for day in testing[1:]:
+        data = datasets.load_svmlight_file(source + "/testData/Day" + str(day) + ".svm", n_features=3300000)
+        test_X = sp.vstack((test_X, data[0]), format='csr')
+        test_y = np.concatenate((test_y, data[1]), axis=0)
+
+    prediction = classifier.predict(test_X)
+    print("Accuracy:", accuracy_score(test_y, prediction))
+    cm = metrics.confusion_matrix(test_y, prediction)
+    print(cm)
+
+
+if __name__ == "__main__":
+    clf = svm.SVC(kernel="linear")
+    classifier_test(clf, "sample")
--- url-identification.py
+++ url-identification.py
@@ -1,14 +1,6 @@
-from matplotlib import pyplot as plt
-import pandas as pd
-import numpy as np
-from sklearn import datasets
 from sklearn import svm
-from sklearn.metrics import accuracy_score
+import classifier_test as ct
 
-train_X, train_y = datasets.load_svmlight_file("url_svmlight/Day17.svm", n_features=3231952)
 clf = svm.SVC(kernel="linear")
-clf.fit(train_X, train_y)
 
-test_X, test_y = datasets.load_svmlight_file("url_svmlight/Day54.svm", n_features=3231952)
-
-print(accuracy_score(test_y, clf.predict(test_X)))
+ct.classifier_test(clf)
