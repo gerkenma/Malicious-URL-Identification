0fb22ccaacdf74369e968a4a15cb99e1191e3b14
gerkenma <mark.gerken@slu.edu>
2018-05-02 00:14:29 -0500

    Ensemble and Voting Classifier
    Classifier using voting between SVM, KNN, random forest classifier.

--- classifier_test.py
+++ classifier_test.py
@@ -2,15 +2,17 @@ from sklearn.metrics import accuracy_score
 import scipy.sparse as sp
 import numpy as np
 from sklearn import datasets
-from sklearn import svm
-from sklearn import metrics
-from sklearn import neighbors
+from sklearn import svm # SVM classifier
+from sklearn import neighbors # KNN classifier
+from sklearn import ensemble # Random forest classifier
+from sklearn import metrics # Confusion matrix
+
 
 TRAIN_LENGTH = 3
 TEST_LENGTH = 1
 
 
-def classifier_test(classifier, source):
+def classifier_test(classifiers, source):
     # training = np.random.random_integers(0, 89, TRAIN_LENGTH)
     training = [0, 1, 2]
     print("Training days:", training)
@@ -21,7 +23,10 @@ def classifier_test(classifier, source):
         y = np.concatenate((y, data[1]), axis=0)
 
     print("Beginning fitting.")
-    classifier.fit(X, y)
+    count = 0
+    for clf in classifiers:
+        print(count)
+        clf.fit(X, y)
     print("Done fitting.")
 
     # testing = np.random.random_integers(90, 120, TEST_LENGTH)
@@ -33,15 +38,20 @@ def classifier_test(classifier, source):
         test_X = sp.vstack((test_X, data[0]), format='csr')
         test_y = np.concatenate((test_y, data[1]), axis=0)
 
-    prediction = classifier.predict(test_X)
+    # Use line below if using voting classifier
+    # prediction = classifiers[len(classifiers)-1].predict(test_X)
+    prediction = classifiers[0].predict(test_X)
     print("Accuracy:", accuracy_score(test_y, prediction))
     cm = metrics.confusion_matrix(test_y, prediction)
     print(cm)
 
 
 if __name__ == "__main__":
-    # clf = svm.SVC(kernel="linear")
-    clf = neighbors.KNeighborsClassifier(n_neighbors=15)
+    # linSVM = svm.SVC(kernel="linear", probability=True)
+    # knn = neighbors.KNeighborsClassifier(n_neighbors=15)
+    # rfc = ensemble.RandomForestClassifier(n_estimators=8, n_jobs=-1)
+    # vote = ensemble.VotingClassifier(estimators=[('svc', linSVM), ('knn', knn), ('rf', rfc)], voting='soft', weights=[3, 1, 1], n_jobs=-1)
+
     # Second argument decides if full sized or trimmed files are used
     # "url_svmlight" for full - "sample" for trimmed
-    classifier_test(clf, "sample")
+    classifier_test("hello!", "sample")
